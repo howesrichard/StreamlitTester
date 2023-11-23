2733a46b7e897ebc75e19d2bbb499c5d6c9f1e4e
howesrichard <82419531+howesrichard@users.noreply.github.com>
2023-11-23 00:57:46 +0000

    Updated to reincorporate matplotlib call

--- helloWorld.py
+++ helloWorld.py
@@ -1,1 +1,16 @@
 print("Hello world")
+
+import streamlit as st
+import random
+import pandas as pd
+import matplotlib.pyplot as plt
+
+names = ['a','b','c','d','e','f','g','h','i','j']
+numbers = [random.randint(1,100) for i in range(10)]
+print(numbers)
+
+# Plot a bar chart with names on x-axis and values on y-axis
+plt.plot([1, 2, 3, 4])
+plt.ylabel('some numbers')
+plt.show()
+
